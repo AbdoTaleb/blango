ace48bd5a785d98ba5b93a8f7766926f4f14aec3
Abdo Taleb <abdo.taleb@telenor.se>
2023-09-30 23:47:24 +0000

    Finish Introduction and Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,7 @@ from blog.models import Tag, Post
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('slug', 'published_at')
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
